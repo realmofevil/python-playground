fc375eff814cab08eca1e6c124cf883c99bbdf01
Krasimir Milushev <38146393+realmofevil@users.noreply.github.com>
2020-09-24 20:09:38 +0300

    Initial commit
    - added functions.py with implemented print, len and fizzbuzz
    - also implemented a pure CSS fizzbuzz solution

---
+++ functions.py
@@ -0,0 +1,50 @@
+import sys
+
+
+def print_me(x):
+    """implementation of a basic print function"""
+    x = str(x) + "\n"
+    return sys.stdout.write(x)
+
+
+def len_me(x):
+    """implementation of a basic length function"""
+    length = 0
+    for _ in x:
+        length += 1
+    return length
+
+
+def fizz_buzz(num):
+    """implementation of the Fizz Buzz algorithm"""
+    fizz = num % 3 == 0
+    buzz = num % 5 == 0
+    fizzbuzz = num % 15 == 0  # shorthand for num % 3 == 0 and num % 5 == 0
+    if fizzbuzz:
+        return "FizzBuzz"
+    elif fizz:
+        return "Fizz"
+    elif buzz:
+        return "Buzz"
+    else:
+        return num
+
+
+# for _ in range(1,101):
+#     print_me(fizz_buzz(_))
+
+
+# create an HTML file and do the FizzBuzz with CSS selectors
+with open("fizzbuzz.html", "a") as the_file:
+    the_file.write("<style>\n" \
+                   "div {display: table-row;}\n" \
+                   "div:nth-child(3n+0) {background-color: green;}\n" \
+                   "div:nth-child(5n+0) {background-color: red;}\n" \
+                   "div:nth-child(15n+0) {background-color: yellow;}\n" \
+                   "</style>\n" \
+                   "<html><body>\n")
+for _ in range(1, 101):
+    with open('fizzbuzz.html', "a") as the_file:
+        the_file.write("<div>" + str(fizz_buzz(_)) + "</div>\n")
+with open("fizzbuzz.html", "a") as the_file:
+    the_file.write("</body></html>")
