5e678f6fd79c170dec92e3d608d75e25fb9b8b00
Krasimir Milushev <38146393+realmofevil@users.noreply.github.com>
2020-11-13 06:59:14 +0200

    refactored and optimized
    it got about 2 seconds faster

--- get_mantis_stats.py
+++ get_mantis_stats.py
@@ -2,8 +2,6 @@ from datetime import datetime, date     # , timedelta
 import json
 import requests
 
-# TODO: oh and try to make everything more readable and neat
-
 bacon_open_url = "http://mantis.com/api/rest/issues?project_id=37&filter_id=100&page_size=900"
 bacon_closed_url = "http://mantis.com/api/rest/issues?project_id=37&filter_id=1115&page_size=170"
 zulu_open_url = "http://mantis.com/api/rest/issues?project_id=36&filter_id=100&page_size=900"
@@ -13,25 +11,21 @@ projects = [bacon_open_url, bacon_closed_url, zulu_open_url, zulu_closed_url]
 
 header = {"Authorization": "token_goes_here"}
 
-
 open_today = 0
 resolved_today = 0
+today = date.today()    # - timedelta(days=1)
 
 for project in projects:
     response = requests.get(project, headers=header)
-    issues = json.loads(response.text)["issues"]    # JSON object to a dict inside a list
-    issue_id = [issue["id"] for issue in issues]    # print(issues[0]["id"], issues[0]["project"]["name"])
+    issues = response.json()["issues"]    # JSON object to a dict inside a list
     if "filter_id=100" in project:
+        issue_id = [issue["id"] for issue in issues]    # print(issues[0]["id"], issues[0]["project"]["name"])
         open_today += len(issue_id)
-    if "filter_id=1115" in project:
+    elif "filter_id=1115" in project:
         get_datetime = [issue["updated_at"] for issue in issues]    # %Y-%m-%dT%H:%M:%S%z ISO 8601 format
-        for issue in get_datetime:
-            resolved_date = datetime.fromisoformat(issue).date()    # make this a list comprehension and .count() it
-            today = date.today()    # - timedelta(days=1)
-            if resolved_date == today:
-                resolved_today += 1
+        resolved_today = [datetime.fromisoformat(issue).date() for issue in get_datetime].count(today)
         with open("project_stats.txt", "a") as stats_file:
-            stats_file.write(f"{date.today().strftime('%d %b %Y')}, Open: {open_today} / Resolved: {resolved_today} -"
+            stats_file.write(f"{today:%d %b %Y}, Open: {open_today} / Resolved: {resolved_today} -"
                              f" {'SimsWorld' if 'project_id=37' in project else 'JA'}\n")
         open_today = 0
         resolved_today = 0
@@ -39,13 +33,13 @@ for project in projects:
 
 # debug helpers
 
-pid = [issue["project"]["id"] for issue in issues]
-status = [issue["status"]["name"] for issue in issues]
-pid_status = list(zip(pid, status))
-issue_filter = [(issue["project"]["id"] == 37 or issue["project"]["id"] == 39)
-                for issue in issues
-                if (issue["resolution"]["name"] == "open" or issue["resolution"]["name"] == "reopened")]
-check = list(zip(pid_status, issue_filter))
+# pid = [issue["project"]["id"] for issue in issues]
+# status = [issue["status"]["name"] for issue in issues]
+# pid_status = list(zip(pid, status))
+# issue_filter = [(issue["project"]["id"] == 37 or issue["project"]["id"] == 39)
+#                 for issue in issues
+#                 if (issue["resolution"]["name"] == "open" or issue["resolution"]["name"] == "reopened")]
+# check = list(zip(pid_status, issue_filter))
 
 
 def prettify(x):
@@ -55,17 +49,3 @@ def prettify(x):
 
 # data = response.json()
 # prettify(data)
-
-# filter per project, including subprojects (something is off with the results by -/+5 )
-zulu_open = [(issue["project"]["id"] == 36 or issue["project"]["id"] == 38 or issue["project"]["id"] == 40)
-             and (issue["resolution"]["name"] == "open" or issue["resolution"]["name"] == "reopened")
-             for issue in issues].count(True)
-bacon_open = [(issue["project"]["id"] == 37 or issue["project"]["id"] == 39)
-              and issue["resolution"]["name"] == "open" or issue["resolution"]["name"] == "reopened"
-              for issue in issues].count(True)
-zulu_closed = [(issue["project"]["id"] == 36 or issue["project"]["id"] == 38 or issue["project"]["id"] == 40)
-               and issue["status"]["name"] == "resolved" or issue["status"]["name"] == "closed"
-               for issue in issues].count(True)
-bacon_closed = [(issue["project"]["id"] == 37 or issue["project"]["id"] == 39)
-                and issue["status"]["name"] == "resolved" or issue["status"]["name"] == "closed"
-                for issue in issues].count(True)
